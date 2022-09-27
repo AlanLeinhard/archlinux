24f9038cc263487e6650ebd65fdc1cda4b105641
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2022-09-27 13:14:24 +0300

    Add files via upload

---
+++ arch1.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+
+# В разработке принимали участие:
+# Алексей Бойко https://vk.com/ordanax
+# Степан Скрябин https://vk.com/zurg3
+# Михаил Сарвилин https://vk.com/michael170707
+# Данил Антошкин https://vk.com/danil.antoshkin
+# Юрий Порунцов https://vk.com/poruncov
+
+loadkeys ru
+setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+
+disk=`expr $1 / 1048576`
+
+echo $disk
+
+efi=550
+
+root=20480
+
+swap=`expr $2 '*' 1024`
+
+home=`expr $disk - $efi - $root - $swap`
+
+echo '2.4 создание разделов'
+(
+  echo g;
+
+  echo n;
+  echo;
+  echo;
+  echo +550M;
+
+  echo n;
+  echo;
+  echo;
+  echo +20G;
+
+  echo n;
+  echo;
+  echo;
+  echo +$home'M';
+
+  echo n;
+  echo;
+  echo;
+  echo;
+
+  echo t;
+  echo 1;
+  echo 1;
+
+  echo t;
+  echo 4;
+  echo 19;
+
+  echo w;
+) | fdisk /dev/$3
+
+echo 'Ваша разметка диска'
+fdisk -l
+
+echo '2.4.2 Форматирование дисков'
+mkfs.fat -F32  /dev/$31
+mkfs.ext4  /dev/$32
+mkfs.ext4  /dev/$33
+mkswap /dev/$34
+
+echo '2.4.3 Монтирование дисков'
+mount /dev/$32 /mnt
+mkdir /mnt/{efi,home}
+mount /dev/$31 /mnt/efi
+mount /dev/$33 /mnt/home
+swapon /dev/$34
+
+echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+echo '3.2 Установка основных пакетов'
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
+
+echo '3.3 Настройка системы'
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# arch-chroot /mnt sh -c arch2.sh $3
+
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh) $3"
