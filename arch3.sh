1d27244352b3d4d46e0a00969a7864d27d56d7a6
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2023-08-24 10:24:17 +0500

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -3,6 +3,8 @@
 echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+echo 'IloveCandy' >> /etc/pacman.conf
+echo 'Color' >> /etc/pacman.conf
 pacman -Syyu --noconfirm
 
             # gnome gdm\
@@ -13,14 +15,16 @@ gui_install="xorg xorg-server xorg-server-devel xorg-xinit xorg-setxkbmap plasma
 #  sudo light -As "sysfs/backlight/amdgpu_bl0" 100
 # volumeicon gxkb
 # nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl
+drivers="nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl lib32-mesa-demos mesa-demos libva-vdpau-driver"
 
 echo 'Ставим иксы и драйвера'
-pacman -S $gui_install --noconfirm
+pacman -S $drivers --noconfirm --needed
+echo 'blacklist nouveau' >> /usr/lib/modprobe.d/nvidia-utils.conf
 
 
 echo 'Cтавим DM'
 # systemctl enable gdm
-systemctl enable sddm
+#systemctl enable sddm
 
 # echo 'gxkb &' > ~/.xinitrc
 # echo 'xset b off' >> ~/.xinitrc
@@ -42,24 +46,23 @@ systemctl enable sddm
 echp 'Ставим AUR'
 sudo pacman -S --needed base-devel git --noconfirm
 
-mkdir Downloads
-mkdir Downloads/tools
-cd Downloads/tools/
+mkdir tools
+cd tools/
 
 git clone https://aur.archlinux.org/yay.git
 
 cd yay/ && makepkg -si && cd ..
 
-sudo pacman -S telegram-desktop firefox docker lshw util-linux-libs wmctrl steam discord audacity krita kdenlive obs-studio mpg123 mpv ntfs-3g wine winetricks protontricks git gvfs ccache grub-customizer neofetch --noconfirm
+sudo pacman -S telegram-desktop firefox docker lshw util-linux-libs wmctrl steam discord audacity krita kdenlive obs-studio mpg123 mpv ntfs-3g wine winetricks git gvfs ccache grub-customizer neofetch --noconfirm
 
-yay -S onlyoffice-bin visual-studio-code-bin ttf-times-new-roman yandex-music-player ventoy-bin anilibria-winmaclinux timeshift gamin --noconfirm
-# yay -S proton-ge-custom-bin gputest portproton protontricks 
+yay -S onlyoffice-bin visual-studio-code-bin ttf-times-new-roman ventoy-bin gamin protontricks downgrade optimus-manager-qt --noconfirm
+# yay -S proton-ge-custom-bin gputest portproton protontricks  yandex-music-player anilibria-winmaclinux timeshift
 #  gnome-browser-connector
 
 # echo 'WaylandEnable=false' >> sudo vim /etc/gdm/custom.conf
 
-sudo mkinitcpio -P
-sudo X -configure
+#sudo mkinitcpio -P
+#sudo X -configure
 # sudo cp /etc/X11/xorg.conf.backup /etc/X11/xorg.conf
 
 sudo groupadd docker
@@ -68,8 +71,15 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
 
+#sudo vim /etc/mkinitcpio.conf 
+#MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm crc32c libcrc32c zlib_deflate btrfs)
+#sudo /etc/default/grub
+#GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"
+
 # sudo docker pull postgres
 # sudo docker pull dpage/pgadmin4
+
+
 
 # sudo docker run -p 80:80 \
 #     -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
